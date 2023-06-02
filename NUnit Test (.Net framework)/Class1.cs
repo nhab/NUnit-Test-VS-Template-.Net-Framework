fc148221551d95d50fda14f69427b1450fb92ff3
nhab <me@b.com>
2023-06-02 15:13:26 +0330

    Add project files.

---
+++ NUnit Test (.Net framework)/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Init()
+        { /* ... */
+        }
+
+        [TearDown]
+        public void Dispose()
+        { /* ... */
+        }
+
+        [Test]
+        public void Test1()
+        { /* ... */ 
+        }
+    }
+}
